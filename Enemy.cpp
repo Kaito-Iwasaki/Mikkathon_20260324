3f327eb24047dd88b18f716c7c26ffdc58518184
Kaito-Iwasaki <251307@st.yoshida-g.ac.jp>
2026-03-24 14:10:54 +0900

    Enemy追加

---
+++ Enemy.cpp
@@ -0,0 +1,161 @@
+//=====================================================================
+//
+// Enemy [Enemy.cpp]
+// Author : 
+// 
+//=====================================================================
+
+//*********************************************************************
+// 
+// ***** インクルードファイル *****
+// 
+//*********************************************************************
+#include "Enemy.h"
+
+//*********************************************************************
+// 
+// ***** マクロ定義 *****
+// 
+//*********************************************************************
+#define TEXTURE_FILENAME	NULL
+#define INIT_POS			D3DXVECTOR3(100.0f, 100.0f, 0.0f)
+#define INIT_SIZE			D3DXVECTOR3(100.0f, 100.0f, 0.0f)
+#define INIT_COLOR			D3DXCOLOR(1, 0, 0, 1)
+
+//*********************************************************************
+// 
+// ***** 構造体 *****
+// 
+//*********************************************************************
+
+
+//*********************************************************************
+// 
+// ***** 列挙型 *****
+// 
+//*********************************************************************
+
+
+//*********************************************************************
+// 
+// ***** プロトタイプ宣言 *****
+// 
+//*********************************************************************
+
+
+//*********************************************************************
+// 
+// ***** グローバル変数 *****
+// 
+//*********************************************************************
+LPDIRECT3DVERTEXBUFFER9 g_pVtxBuffEnemy = NULL;
+LPDIRECT3DTEXTURE9 g_pTexBuffEnemy = NULL;
+ENEMY g_aEnemy[MAX_ENEMY];
+
+//=====================================================================
+// 初期化処理
+//=====================================================================
+void InitEnemy(void)
+{
+	LPDIRECT3DDEVICE9 pDevice = GetDevice();
+
+	// 構造体の初期化
+	memset(&g_aEnemy, 0, sizeof(ENEMY) * MAX_ENEMY);
+	for (int i = 0; i < MAX_ENEMY; i++)
+	{
+		g_aEnemy[i].obj.pos = INIT_POS;
+		g_aEnemy[i].obj.size = INIT_SIZE;
+		g_aEnemy[i].obj.color = INIT_COLOR;
+		g_aEnemy[i].obj.bVisible = true;
+	}
+
+	// テクスチャの読み込み
+	if (TEXTURE_FILENAME)
+	{
+		D3DXCreateTextureFromFile(
+			pDevice,
+			TEXTURE_FILENAME,
+			&g_pTexBuffEnemy
+		);
+	}
+
+	// 頂点バッファの生成
+	pDevice->CreateVertexBuffer(
+		sizeof(VERTEX_2D) * 4 * MAX_ENEMY,
+		D3DUSAGE_WRITEONLY,
+		FVF_VERTEX_2D,
+		D3DPOOL_MANAGED,
+		&g_pVtxBuffEnemy,
+		NULL
+	);
+
+}
+
+//=====================================================================
+// 終了処理
+//=====================================================================
+void UninitEnemy(void)
+{
+	if (g_pTexBuffEnemy != NULL)
+	{// テクスチャの破棄
+		g_pTexBuffEnemy->Release();
+		g_pTexBuffEnemy = NULL;
+	}
+
+	if (g_pVtxBuffEnemy != NULL)
+	{// 頂点バッファの破棄
+		g_pVtxBuffEnemy->Release();
+		g_pVtxBuffEnemy = NULL;
+	}
+}
+
+//=====================================================================
+// 更新処理
+//=====================================================================
+void UpdateEnemy(void)
+{
+
+}
+
+//=====================================================================
+// 描画処理
+//=====================================================================
+void DrawEnemy(void)
+{
+	LPDIRECT3DDEVICE9 pDevice = GetDevice();
+	VERTEX_2D* pVtx;
+
+	// 頂点バッファをロックして頂点情報へのポインタを取得
+	g_pVtxBuffEnemy->Lock(0, 0, (void**)&pVtx, 0);
+
+	// 頂点情報を設定
+	for (int i = 0; i < MAX_ENEMY; i++, pVtx += 4)
+	{
+		if (!g_aEnemy[i].obj.bVisible || !g_aEnemy[i].bUsed) continue;
+
+		SetVertexPos(pVtx, g_aEnemy[i].obj);
+		SetVertexRHW(pVtx, 1.0f);
+		SetVertexColor(pVtx, g_aEnemy[i].obj.color);
+		SetVertexTexturePos(pVtx);
+	}
+
+	// 頂点バッファをアンロック
+	g_pVtxBuffEnemy->Unlock();
+
+	// 頂点バッファをデータストリームに設定
+	pDevice->SetStreamSource(0, g_pVtxBuffEnemy, 0, sizeof(VERTEX_2D));
+
+	// 頂点フォーマットの設定
+	pDevice->SetFVF(FVF_VERTEX_2D);
+
+	for (int i = 0; i < MAX_ENEMY; i++)
+	{
+		if (!g_aEnemy[i].obj.bVisible || !g_aEnemy[i].bUsed) continue;
+
+		// テクスチャの設定
+		pDevice->SetTexture(0, NULL);
+
+		// ポリゴンの描画
+		pDevice->DrawPrimitive(D3DPT_TRIANGLESTRIP, i * 4, 2);
+	}
+}
